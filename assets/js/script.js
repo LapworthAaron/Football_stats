dc42852b307dd8995f5d80f9e9d32164131cf83b
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-02-02 12:02:28 +0000

    dynamic creation of table contents

--- assets/js/script.js
+++ assets/js/script.js
@@ -55,5 +55,38 @@ function leagueTableTemplate() {
 
 // dynamically create the table contents, looping through the response object array
 function leagueTableContents() {
-    
+    for (var i = 0; i < response.table.length; i++) {
+        var tableRow = $('<tr>');
+        var pos = $('<td>').text(response.table[i].intRank).attr('class','position rowPosition');
+        var logo = $('<td>').attr('class','logo');
+        var logoH = $('<img>').attr({'src':response.table[i].strTeamBadge,
+                                    'width':'25px'});
+        logo.append(logoH);
+        var team = $('<td>').text(response.table[i].strTeam).attr('class','team');
+        var played =$('<td>').text(response.table[i].intPlayed).attr('class','played');
+        var wins =$('<td>').text(response.table[i].intWin).attr('class','wins');
+        var draws =$('<td>').text(response.table[i].intDraw).attr('class','draws');
+        var loses =$('<td>').text(response.table[i].intLoss).attr('class','loses');
+        var gd =$('<td>').text(response.table[i].intGoalDifference).attr('class','goalDiff');
+        var gf =$('<td>').text(response.table[i].intGoalsFor).attr('class','gf');
+        var ga =$('<td>').text(response.table[i].intGoalsAgainst).attr('class','ga');
+        var points =$('<td>').text(response.table[i].intPoints).attr('class','points');
+        var form =$('<td>').attr('class','form');
+        //for loop to create individual elements for each game in the form, so we can have conditional formatting
+        for (var j = 0; j < 5; j++){
+            var div = $('<div>');
+            if (response.table[i].strForm[j] === 'W') {
+                div.text(response.table[i].strForm[j]).attr('class','formGreen');
+            } else if (response.table[i].strForm[j] === 'L') {
+                div.text(response.table[i].strForm[j]).attr('class','formRed');
+            } else {
+                div.text(response.table[i].strForm[j]);
+            }
+            form.append(div);
+        }
+        var desc =$('<td>').text(response.table[i].strDescription).attr('class','info');
+        $('#dynamicTable').append(tableRow);
+        logo.append(logoH);
+        tableRow.append(pos,logo,team,played,wins,draws,loses,gd,gf,ga,points,form,desc);
+    }
 }
