d5b035bab8c82dd221b206baa55d0859e4ed4812
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-02-02 11:39:04 +0000

    league lookup array and api url

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+var leagues = {'English_Premier_League':4328,
+               'The_Championship':4329,
+               'League_One':4396,
+               'League_Two':4397};
+
+var tableUrl = 'https://www.thesportsdb.com/api/v1/json/3/lookuptable.php?';
