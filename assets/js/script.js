6ed28cd16a7bed3d1b79e696dcebd012fa675f4a
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-02-02 11:56:49 +0000

    table template code

--- assets/js/script.js
+++ assets/js/script.js
@@ -28,7 +28,29 @@ function leagueTable() {
 
 // create the base table (headers), no actual data content
 function leagueTableTemplate() {
+    // create table and append to league table section
+    var table = $('<table>').attr('id','dynamicTable');
+    $('#leagueTable').append(table);
 
+    // create all the headers for the table and append to the table
+    var header = $('<tr>');
+    table.append(header);
+    var pos = $('<th>').text('Position').attr('class','position');
+    var logo = $('<th>').text('Logo').attr('class','logo');
+    var team = $('<th>').text('Team').attr('class','team');
+    var played =$('<th>').text('Played').attr('class','played');
+    var wins =$('<th>').text('Wins').attr('class','wins');
+    var draws =$('<th>').text('Draws').attr('class','draws');
+    var loses =$('<th>').text('Loses').attr('class','loses');
+    var gd =$('<th>').text('Goal Diff').attr('class','goalDiff');
+    var gf =$('<th>').text('GF').attr('class','gf');
+    var ga =$('<th>').text('GA').attr('class','ga');
+    var points =$('<th>').text('Points').attr('class','points');
+    var form =$('<th>').text('Form').attr('class','form');
+    var desc =$('<th>').text('Info').attr('class','info');
+    
+    header.append(pos,logo,team,played,wins,draws,loses,gd,gf,ga,points,form,desc);
+    return;    
 }
 
 // dynamically create the table contents, looping through the response object array
