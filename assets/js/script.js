5cb54222bfb7d006c567fe7255bfdc815935500d
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-02-02 11:45:47 +0000

    api call structure

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,4 +13,25 @@ function leagueTable() {
     var queryUrl = tableUrl + league + year;
 
     //ajax api call
+    $.ajax({"url": queryUrl,
+            "method": "GET"
+        })
+    .then(function (response) {
+        //create table and append to the league table section
+        leagueTableTemplate(response);
+        // dynamically create the table contents
+        leagueTableContents(response);
+    });
+    // change class to display the table section
+    $('#leagueTable').empty().attr('class','show');
+}
+
+// create the base table (headers), no actual data content
+function leagueTableTemplate() {
+
+}
+
+// dynamically create the table contents, looping through the response object array
+function leagueTableContents() {
+    
 }
