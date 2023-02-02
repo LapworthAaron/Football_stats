41165f7ce0b06135c16d2f16dca25f24d5dd19c2
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-02-02 11:40:18 +0000

    added api url construction

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,13 @@ var leagues = {'English_Premier_League':4328,
                'League_Two':4397};
 
 var tableUrl = 'https://www.thesportsdb.com/api/v1/json/3/lookuptable.php?';
+
+// create and display the league table dynamically
+function leagueTable() {
+    // set the api url
+    var league = 'l=' + leagues[$('#leagues').val()];
+    var year = '&s=' + $('#season').val();
+    var queryUrl = tableUrl + league + year;
+
+    //ajax api call
+}
